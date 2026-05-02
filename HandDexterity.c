333f584254f0c10de6cbde5cbe7ad46ab4f6b4b9
marina-medeiros <marinamaleite@gmail.com>
2026-05-02 02:01:32 -0300

    fix: setup display with all functions

--- HandDexterity.c
+++ HandDexterity.c
@@ -7,28 +7,12 @@
 #include "include/leds.h"
 #include "include/joystick.h"
 
-#define I2C_PORT i2c1
-#define I2C_SDA 15
-#define I2C_SCL 14
-
 void setup(){
-    //inicialização do Oled
     stdio_init_all();
-    i2c_init(I2C_PORT, 400*1000);
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-
     setup_display();
-
-    //inicializa leds
     setup_leds();
-
-    //inicialização do botão do joystick
     setup_joystick();
 }
- 
 
 int main(){
     setup();
--- src/menu.c
+++ src/menu.c
@@ -6,8 +6,16 @@
 
 ssd1306_t disp; 
 #define I2C_PORT i2c1
+#define I2C_PORT i2c1
+#define I2C_SDA 15
+#define I2C_SCL 14
 
 void setup_display(){
+    i2c_init(I2C_PORT, 400*1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
     disp.external_vcc=false;
     ssd1306_init(&disp, 128, 64, 0x3C, I2C_PORT);
     ssd1306_clear(&disp);
